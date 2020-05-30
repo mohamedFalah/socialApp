0fc76d2f471c9796a862942de3993983b7399c0a
MohamedFalah <mohamshualh@gmail.com>
2020-05-31 01:15:12 +0300

    new project with login UI

---
+++ socialApp/controllers/SignInVC.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  socialApp
+//
+//  Created by Mohammed Alshulah on 30/05/2020.
+//  Copyright © 2020 Mohammed Alshulah. All rights reserved.
+//
+
+import UIKit
+//import MaterialComponents.MaterialTextControls_OutlinedTextAreas
+
+
+class SignInVC: UIViewController {
+
+    @IBOutlet weak var EmailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        let estimatedFrame = ....
+//        let textField = MDCOutlinedTextField(frame: estimatedFrame)
+//        textField.label.text = "Phone number"
+//        textField.placeholder = "555-555-5555"
+//        textField.leadingAssistiveLabel.text = "This is helper text"
+//        textField.sizeToFit()
+//        view.addSubview(textField)
+    }
+    
+    
+
+
+}
+
